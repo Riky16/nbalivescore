c0359fd1e5d08c29db0e00aca1e813ca91f188b1
Riccardo Donati <riky.donati16@gmail.com>
2020-12-19 18:02:44 +0100

    feat: auto update page

--- js/index.js
+++ js/index.js
@@ -2,6 +2,18 @@
 
 // });
 
+
+setInterval(
+  function(){ 
+    $.getJSON(apiUrl, getEventsByDate);
+    
+   }, 10000);
+
+
+
+// Update this with the numer of events
+let gameEvents = 0;
+
 let date = "&date=20201225"
 let apiUrl = "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard";
 
@@ -10,37 +22,33 @@ $.ajax({
     url: apiUrl,
     data: "",
     success: function () {
-      console.log("Request Completed - Update Information [" + date + "]");
+      console.log("Request Completed - Update Information ");
     },
     statusCode: {
       400: function () {
         alert("Richiesta non valida");
       },
       404: function () {
-        alert("Nome della città errato");
+        alert("Dati non corretti");
       },
     },
 });
 
-$.getJSON(apiUrl, getEventsByDate);
 
 function getEventsByDate(data) {
-    let homeAway = data.events[0].competitions[0].competitors[0].homeAway
-    let homeTeam = homeAway == "home" ? data.events[0].competitions[0].competitors[0].team.displayName : ""
-    let homeTeamLogo = homeAway == "home" ? data.events[0].competitions[0].competitors[0].team.logo : ""    
-    homeAway = data.events[0].competitions[0].competitors[1].homeAway
-    let awayTeam = homeAway == "away" ? data.events[0].competitions[0].competitors[1].team.displayName : ""
-    let awayTeamLogo = homeAway == "away" ? data.events[0].competitions[0].competitors[1].team.logo : ""
+    let homeAway = data.events[gameEvents].competitions[0].competitors[0].homeAway
+    let homeTeam = homeAway == "home" ? data.events[gameEvents].competitions[0].competitors[0].team.displayName : ""
+    let homeTeamLogo = homeAway == "home" ? data.events[gameEvents].competitions[0].competitors[0].team.logo : ""    
+    homeAway = data.events[gameEvents].competitions[0].competitors[1].homeAway
+    let awayTeam = homeAway == "away" ? data.events[gameEvents].competitions[0].competitors[1].team.displayName : ""
+    let awayTeamLogo = homeAway == "away" ? data.events[gameEvents].competitions[0].competitors[1].team.logo : ""
     
     $(nameHome).text(homeTeam)
     $(nameAway).text(awayTeam)
     $(imgHome).attr('src', homeTeamLogo);
     $(imgAway).attr('src', awayTeamLogo);
 
-    // document.getElementById("test").innerHTML = data.events[0].shortName;
 
-    $(casa).text(data.events[0].competitions[0].competitors[0].score)
-    // document.getElementById("casa").innerHTML = data.events[0].competitions[0].competitors[0].score;
-    // document.getElementById("trasferta").innerHTML = data.events[0].competitions[0].competitors[1].score;
-    $(trasferta).text(data.events[0].competitions[0].competitors[1].score)
+    $(casa).text(data.events[gameEvents].competitions[0].competitors[0].score)
+    $(trasferta).text(data.events[gameEvents].competitions[0].competitors[1].score)
 }
