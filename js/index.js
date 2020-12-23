e081602fe162fb20005ba1af4064c24929a4ef0c
Riky <riky.donati16@gmail.com>
2020-12-23 01:55:49 +0100

    feat: add leader stats

--- js/index.js
+++ js/index.js
@@ -57,7 +57,55 @@ function getEventsByDate(data) {
     $(scoreAway).text(data.events[gameEvents].competitions[0].competitors[1].score)
 
     // Stats
-    // let topscoreHome = data.events[gameEvents].competitions[0].competitors[0].team.links[2].href
-    // $(test).text(displayClock)
+    // Points Home
+    let topscorePnameHome = data.events[gameEvents].competitions[0].competitors[1].leaders[0].leaders[0].athlete.fullName
+    let topscorePHome = data.events[gameEvents].competitions[0].competitors[1].leaders[0].leaders[0].value
+    let topscorePphotoHome = data.events[gameEvents].competitions[0].competitors[1].leaders[0].leaders[0].athlete.headshot
+    console.log(topscorePnameHome)
+    $(pNameHome).text(topscorePnameHome)
+    $(pScoreHome).text(topscorePHome)
+    $(pPhotoHome).attr('src', topscorePphotoHome)
+    // Points Away
+    let topscorePnameAway = data.events[gameEvents].competitions[0].competitors[0].leaders[0].leaders[0].athlete.fullName
+    let topscorePAway = data.events[gameEvents].competitions[0].competitors[0].leaders[0].leaders[0].value
+    let topscorePphotoAway = data.events[gameEvents].competitions[0].competitors[0].leaders[0].leaders[0].athlete.headshot
+    console.log(topscorePnameAway)
+    $(pNameAway).text(topscorePnameAway)
+    $(pScoreAway).text(topscorePAway)
+    $(pPhotoAway).attr('src', topscorePphotoAway)
+
+  // Rebounds Home
+    let topscoreRnameHome = data.events[gameEvents].competitions[0].competitors[1].leaders[1].leaders[0].athlete.fullName
+    let topscoreRHome = data.events[gameEvents].competitions[0].competitors[1].leaders[1].leaders[0].value
+    let topscoreRphotoHome = data.events[gameEvents].competitions[0].competitors[1].leaders[1].leaders[0].athlete.headshot
+    console.log(topscoreRnameHome)
+    $(rNameHome).text(topscoreRnameHome)
+    $(rScoreHome).text(topscoreRHome)
+    $(rPhotoHome).attr('src', topscoreRphotoHome)
+    // Rebounds Away
+    let topscoreRnameAway = data.events[gameEvents].competitions[0].competitors[0].leaders[1].leaders[0].athlete.fullName
+    let topscoreRAway = data.events[gameEvents].competitions[0].competitors[0].leaders[1].leaders[0].value
+    let topscoreRphotoAway = data.events[gameEvents].competitions[0].competitors[1].leaders[1].leaders[0].athlete.headshot
+    console.log(topscoreRnameAway)
+    $(rNameAway).text(topscoreRnameAway)
+    $(rScoreAway).text(topscoreRAway)
+    $(rPhotoAway).attr('src', topscoreRphotoAway)
+
+  // Assist Home
+  let topscoreAnameHome = data.events[gameEvents].competitions[0].competitors[1].leaders[2].leaders[0].athlete.fullName
+  let topscoreAHome = data.events[gameEvents].competitions[0].competitors[1].leaders[2].leaders[0].value
+  let topscoreAphotoHome = data.events[gameEvents].competitions[0].competitors[1].leaders[2].leaders[0].athlete.headshot
+  console.log(topscoreAnameHome)
+  $(aNameHome).text(topscoreAnameHome)
+  $(aScoreHome).text(topscoreAHome)
+  $(aPhotoHome).attr('src', topscoreAphotoHome)
+  // Assist Away
+  let topscoreAnameAway = data.events[gameEvents].competitions[0].competitors[0].leaders[2].leaders[0].athlete.fullName
+  let topscoreAAway = data.events[gameEvents].competitions[0].competitors[0].leaders[2].leaders[0].value
+  let topscoreAphotoAway = data.events[gameEvents].competitions[0].competitors[1].leaders[2].leaders[0].athlete.headshot
+  console.log(topscoreRnameAway)
+  $(aNameAway).text(topscoreAnameAway)
+  $(aScoreAway).text(topscoreAAway)
+  $(aPhotoAway).attr('src', topscoreAphotoAway)
 
 }
